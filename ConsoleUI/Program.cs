a541bc69e6bd70567e238e3bf1a0847060c4c179
davidcol1 <davidmcolvin@gmail.com>
2019-12-09 13:20:10 -0500

    Start to refactor the brownfield app

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -26,62 +26,9 @@ namespace ConsoleUI
       string w, rawTimeWorked;
       int i;
       double ttl, t;
-      List<TimeSheetEntry> ents = new List<TimeSheetEntry>();
-      Console.Write("Enter what you did: ");
-      w = Console.ReadLine();
-      Console.Write("How long did you do it for: ");
-      rawTimeWorked = Console.ReadLine();
 
-      while (double.TryParse(rawTimeWorked, out t) == false)
-      {
-        Console.WriteLine();
-        Console.WriteLine("Invalid number given");
-        Console.Write("How long did you do it for: ");
-        rawTimeWorked = Console.ReadLine();
-      }
+      List<TimeSheetEntry> ents = LoadTimeSheets();
 
-      TimeSheetEntry ent = new TimeSheetEntry();
-      ent.HoursWorked = t;
-      ent.WorkDone = w;
-      ents.Add(ent);
-      Console.Write("Do you want to enter more time (yes/no): ");
-
-      string answer = Console.ReadLine();
-      bool cont = false;
-
-      if (answer.ToLower() == "yes")
-      {
-        cont = true;
-      }
-
-      while (cont == true)
-      {
-        Console.Write("Enter what you did: ");
-        w = Console.ReadLine();
-        Console.Write("How long did you do it for: ");
-        rawTimeWorked = Console.ReadLine();
-
-        while (double.TryParse(rawTimeWorked, out t) == false)
-        {
-          Console.WriteLine();
-          Console.WriteLine("Invalid number given");
-          Console.Write("How long did you do it for: ");
-          rawTimeWorked = Console.ReadLine();
-        }
-
-        ent = new TimeSheetEntry();
-        ent.HoursWorked = t;
-        ent.WorkDone = w;
-        ents.Add(ent);
-        Console.Write("Do you want to enter more time (yes/no): ");
-        answer = Console.ReadLine();
-        cont = false;
-
-        if (answer.ToLower() == "yes")
-        {
-          cont = true;
-        }
-      } 
       ttl = 0;
       for (i = 0; i < ents.Count; i++)
       {
@@ -120,6 +67,37 @@ namespace ConsoleUI
       Console.WriteLine();
       Console.Write("Press any key to exit application...");
       Console.ReadKey();
+    }
+
+    private static List<TimeSheetEntry> LoadTimeSheets()
+    {
+      List<TimeSheetEntry> output = new List<TimeSheetEntry>();
+      string enterMoreTimeSheets = "";
+      do
+      {
+        Console.Write("Enter what you did: ");
+        string workDone = Console.ReadLine();
+        Console.Write("How long did you do it for: ");
+        string rawHoursWorked = Console.ReadLine();
+        double hoursWorked;
+
+        while (double.TryParse(rawHoursWorked, out hoursWorked) == false)
+        {
+          Console.WriteLine();
+          Console.WriteLine("Invalid number given");
+          Console.Write("How long did you do it for: ");
+          rawHoursWorked = Console.ReadLine();
+        }
+
+        TimeSheetEntry timeSheet = new TimeSheetEntry();
+        timeSheet.HoursWorked = hoursWorked;
+        timeSheet.WorkDone = workDone;
+        output.Add(timeSheet);
+        Console.Write("Do you want to enter more time (yes/no): ");
+        enterMoreTimeSheets = Console.ReadLine();
+      } while (enterMoreTimeSheets == "yes");
+
+      return output;
     }
   }
 
